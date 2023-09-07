f13db9b3395560d253965916cb833e6a3c316d57
rubiyadav <rubiflyeis@gmail.com>
2023-09-07 12:31:02 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,7 @@ exports.forgetPassword = async (req, res) => {
                         let otpExpiration = new Date(Date.now() + 5 * 60 * 1000);
                         const updated = await User.findOneAndUpdate({ _id: data._id }, { $set: { accountVerification: accountVerification, otp: otp, otpExpiration: otpExpiration } }, { new: true, });
                         if (updated) {
-                                return res.status(200).json({ message: "Otp send to your email.", status: 200, data: {} });
+                                return res.status(200).json({ message: "Otp send to your email.", status: 200, data: updated });
                         }
                 }
         } catch (err) {
