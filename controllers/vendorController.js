aedad2d85c8da9bbc60576dcd71447bfd8eeafba
rubiyadav <rubiflyeis@gmail.com>
2023-09-16 14:48:29 +0530

    update

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1234,7 +1234,7 @@ exports.getcancelReturnOrder = async (req, res, next) => {
                 return res.status(200).json({ status: 200, msg: "orders of user", data: orders })
         } catch (error) {
                 console.log(error);
-                res.status(501).send({ status: 501, message: "server error.", data: {}, });
+                return res.status(501).send({ status: 501, message: "server error.", data: {}, });
         }
 };
 exports.addOffer = async (req, res) => {
